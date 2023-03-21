654951571c2f595db24ae24a29710c90425c0932
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-03-21 16:50:04 +0530

    include form to take values

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head></head>
+<body>
+
+    <form action="insert.php" method="post">
+        Name: <input type="text" name="name"><br>
+        Email: <input type="text" name="email"><br>
+        <input type="submit" value="Submit">
+    </form>
+    
+</body>
+</html>
