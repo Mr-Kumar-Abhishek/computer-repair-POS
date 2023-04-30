690dec98987cfd777c428e48dd78ace1320b15bf
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-04-30 22:33:03 +0530

    bug fix

--- make-super-admin.php
+++ make-super-admin.php
@@ -22,7 +22,7 @@ $name = $_POST ["name"];
 $email = $_POST ["email"];
 
 // Insert data into database
-$sql = "INSERT INTO super_admin (name, email) VALUES ('$name', '$email')";
+$sql = "INSERT INTO super_admin (username, email) VALUES ('$name', '$email')";
 
 if ($conn->query ($sql) === TRUE) {
   echo "New record created successfully";
