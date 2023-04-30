166e072faad41b528ccda31da47768d317c09641
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-04-30 22:20:47 +0530

    make super admin script

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <head></head>
 <body>
 
-    <form action="insert.php" method="post">
+    <form action="make-super-admin.php" method="post">
         Name: <input type="text" name="name"><br>
         Email: <input type="text" name="email"><br>
         <input type="submit" value="Submit">
