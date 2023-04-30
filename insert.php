743cc9cbd5b26d469977303e55ef6974bf1c6e94
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-04-30 22:08:52 +0530

    insert email and name into database

--- insert.php
+++ insert.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection
 $servername = "localhost";
-$username = "username";
-$password = "password";
-$dbname = "database_name";
+$username = "root";
+$password = "leptonification";
+$dbname = "laptop_world";
 
 // Create connection
 $conn = new mysqli ($servername, $username, $password, $dbname);
@@ -18,7 +18,7 @@ $name = $_POST ["name"];
 $email = $_POST ["email"];
 
 // Insert data into database
-$sql = "INSERT INTO table_name (name, email) VALUES ('$name', '$email')";
+$sql = "INSERT INTO super_admin (name, email) VALUES ('$name', '$email')";
 
 if ($conn->query ($sql) === TRUE) {
   echo "New record created successfully";
