71f4280460a776d6018a87d3dfe8af98d215e319
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-05-01 22:38:43 +0530

    clear files

--- db-disconnect.php
+++
@@ -1,4 +0,0 @@
-<?php
-    // Close connection
-    $conn->close ();
-?>
--- laptop_world.sql
+++
@@ -1,1 +0,0 @@
-create table super_admin(SAID int primary key auto_increment, username varchar(255) not null, email varchar(255) not null );
