1d54adae78d29662a26656dc2c41d44eb61a04b0
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-05-02 14:59:52 +0530

    set brand for the page

--- views/modules/header.php
+++ views/modules/header.php
@@ -11,5 +11,21 @@
         <a href="#" class="nav-link">Contact</a>
       </li>
     </ul>
-
+    <!-- Right navbar links -->
+    <ul class="navbar-nav ml-auto">
+        <li class="nav-item">
+            <a class="nav-link" data-widget="fullscreen" href="#" role="button">
+              <i class="fas fa-expand-arrows-alt"></i>
+            </a>
+        </li>
+    </ul>
 </nav>
+
+  <!-- Main Sidebar Container -->
+  <aside class="main-sidebar sidebar-dark-primary elevation-4">
+    <!-- Brand Logo -->
+    <a href="index3.html" class="brand-link">
+      <img src="views/dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <span class="brand-text font-weight-light">POS</span>
+    </a>
+  </aside>
