ecfe875fe70713e3c10ee91bc4b998d4f369ed3c
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-05-02 15:02:30 +0530

    include logout link

--- views/modules/header.php
+++ views/modules/header.php
@@ -10,6 +10,9 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a href="#" class="nav-link">logout</a>
+      </li>
     </ul>
     <!-- Right navbar links -->
     <ul class="navbar-nav ml-auto">
