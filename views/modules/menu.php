011ac9f1be99ec3d5906d7220b3a75b05547d844
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-05-06 18:26:26 +0530

    fix html bug

--- views/modules/menu.php
+++ views/modules/menu.php
@@ -47,6 +47,7 @@
                             jobs
                         </p>
                 </a>
+               </li>
                <li class="nav-item">
                 <a href="#" class="nav-link">
                     <i class="nav-icon fas fa-people-carry"></i>
