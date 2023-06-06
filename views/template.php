049f669bf57d0cd6a02e2364de024029855228e9
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-06-07 03:47:35 +0530

    include datatables and plugins js

--- views/template.php
+++ views/template.php
@@ -78,5 +78,19 @@
 <!-- AdminLTE for demo purposes -->
 <script src="views/dist/js/demo.js"></script>
 <script src="views/js/template.js"></script>
+
+<!-- DataTables  & Plugins -->
+<script src="views/plugins/datatables/jquery.dataTables.min.js"></script>
+<script src="views/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+<script src="views/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+<script src="views/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+<script src="views/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+<script src="views/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+<script src="views/plugins/jszip/jszip.min.js"></script>
+<script src="views/plugins/pdfmake/pdfmake.min.js"></script>
+<script src="views/plugins/pdfmake/vfs_fonts.js"></script>
+<script src="views/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+<script src="views/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+<script src="views/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
 </body>
 </html>
