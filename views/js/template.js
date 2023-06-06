26e3780f9aa6a55d85ce13bba067b68d65e7bb80
Abhishek Kumar <mr.kumar.abhishek@email.com>
2023-06-07 05:21:06 +0530

    bug fix trial

--- views/js/template.js
+++ views/js/template.js
@@ -1,6 +1,4 @@
-import DataTable from 'datatables.net-dt';
-import 'datatables.net-responsive-dt';
 
-let table = new DataTable('.user-table', {
-    responsive: true
-});
+$(".user-table").DataTable({
+    responsive: true    
+})
